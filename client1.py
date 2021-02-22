ebfd86c83f36ceaa23e18f775e52936609e95634
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2021-02-22 15:02:38 +0900

    upload client1
    clt

---
+++ client1.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+
+# make soket
+new_soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# localhost
+host = 'localhost'
+# port 
+port = 8080
+# konek ke server via localhost
+server_id = ((host, port))
+print("Terhubung")
+new_soket.connect(server_id)
+# reply ke srv
+message = 'message'
+new_soket.sendall(message)
+# respone
+data = new_soket.recv(10)
+print(data)
+print("Socket closed")
+new_soket.close()
