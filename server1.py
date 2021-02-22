17d382481d3e402c6179b109488585af456e8f51
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2021-02-22 15:01:44 +0900

    upload server1.py
    srv

---
+++ server1.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+# make tcp/ip socket dengan address family
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# localohost
+host = 'localhost'
+# make port
+port = 8080
+# bindng dari socket ke port
+sock.bind((host, port))
+# listen the connection
+sock.listen(1)
+# waiting for connection
+print("Wait for the connection")
+connection, client = sock.accept()
+print(client, "connected")
+# data di terima
+data = connection.recv(16)
+print("Received %s" % data)
+if data:
+    connection.sendall(data)
+else :
+    ("no data from", client)
+# close connection
+connection.close()    
+
