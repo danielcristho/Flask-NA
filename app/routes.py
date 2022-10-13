015df627d0a5e29654a6bd2445884da27f49a648
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-10-13 17:20:29 +0700

    feat: update requirement

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import db, app
 from flask import jsonify, request, url_for
 from app.models import Device, ValidationError
-
+from automate import show_version
 def get_url(self):
     return url_for('get_device', id=self.id, _external=True)
 
@@ -30,21 +30,46 @@ def import_data(self, data):
 
 @app.route('/devices/', methods=['GET'])
 def get_devices():
-    return jsonify({'device': [device.get_url()
-                    for device in Device.query.all()]})
-    
+    return jsonify({'device': [device.get_url() 
+                            for device in Device.query.all()]})
+
 
 @app.route('/devices/<int:id>', methods=['GET'])
 def get_device(id):
     return jsonify(Device.query.get_or_404(id).export_data())
 
-app.route('/devices/<int:id>', methods=['POST'])
+
+@app.route('/devices/<int:id/version>', methods=['GET'])
+def get_device_version(id):
+    device = Device.query.get_or_404(id)
+    hostname = device.hostname
+    ip = device.mgmt_ip
+    prompt = hostname+"#"
+    result = show_version(hostname, prompt, ip, 'cisco', 'cisco')
+    return jsonify({"version": str(result)})
+
+@app.route('/devices/<device_role/version>', methods=['GET'])
+def get_role_version(device_role):
+    device_id_list = [device.id for device in Device.query.all() if device.role == device_role]
+    result = {}
+    for id in device_id_list:
+        device = Device.query.get_or_404(id)
+        hostname = device.hostname
+        ip = device.mgmt_ip
+        prompt = hostname + "#"
+        device_result = show_version(hostname, prompt, ip, 'cisco', 'cisco')
+        result[hostname] = str(device_result)
+    return jsonify(result)    
+
+
+@app.route('/devices/', methods=['POST'])
 def new_device():
     device = Device()
     device.import_data(request.json)
     db.session.add(device)
     db.session.commit()
     return jsonify({}), 201, {'Location': device.get_url()}
+
 
 @app.route('/devices/<int:id>', methods=['PUT'])
 def edit_device(id):
