6664c5456f638e7d993b4a10a106183a88b0c5d0
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-01-18 14:46:49 +0700

    chore: enable multithreading to send config

--- nornir_automation.py
+++ nornir_automation.py
@@ -1,14 +1,22 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
-from nornir_netmiko import netmiko_send_command
+from nornir_netmiko.tasks import netmiko_send_config
+from nornir_netmiko.tasks import netmiko_send_command
 
-#from nornir.plugins.functions.text import print_result
+nr = InitNornir(config_file="config.yaml")
 
-nr = InitNornir()
+commands = ['interface loopback 0 ', 'ip add 1.1.1.1 255.255.255.255', 'no shutdown']
 
-result = nr.run(
-    task=netmiko_send_command,
-    command_string  ="show ip interface brief"
-)
-
+def send_task(task):
+    task.run(
+        task=netmiko_send_config,
+        config_commands=commands,
+    )
+result=nr.run(task=send_task)
 print_result(result)
+
+command_result = nr.run(
+    task=netmiko_send_command,
+    command_string  ="show ip interface brief | exclude unassigned"
+)
+print_result(command_result)
